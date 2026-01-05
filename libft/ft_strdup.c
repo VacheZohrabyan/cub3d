9c9dc03bdbc6f55affc20ccc52040888154f6343
Vache Zohrabyan <vzohraby@c2r6s4.42yerevan.am>
2026-01-05 16:12:03 +0400

    add

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: vzohraby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 17:07:43 by vzohraby          #+#    #+#             */
-/*   Updated: 2025/01/29 13:51:50 by vzohraby         ###   ########.fr       */
+/*   Updated: 2026/01/05 16:10:45 by vzohraby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,12 @@ char	*ft_strdup(const char *s1)
 {
 	size_t	s_len;
 	char	*ptr;
-	size_t	i;
 
 	s_len = ft_strlen(s1);
 	ptr = (char *)malloc((s_len + 1) * sizeof(char));
 	if (!ptr)
 		return (NULL);
-	i = 0;
-	while (i < s_len + 1)
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	ptr[i] = '\0';
+	ft_memcpy(ptr, s1, s_len);
+	ptr[s_len] = '\0';
 	return (ptr);
 }
